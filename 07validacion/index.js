7279d748c20e5bba93702e1a73dd0ad4e71312af
Stiven Garzon <usg210802@outlook.com>
2025-01-19 16:04:10 -0500

    Creacion de esquema y conexion a base de datos

--- 07validacion/index.js
+++ 07validacion/index.js
@@ -1,1 +1,26 @@
+import express from 'express';
+import 'dotenv/config';
+import { connectDb } from './dataBase/dbConnection.js';
+import Usuarios from './schemas/schemaUser.js';
+
+connectDb();
+const app = express();
+
+app.get('/usuarios', async (req, res) => {
+  const user = await Usuarios.find();
+
+  if (user.lenght === 0) {
+    res.status(404).json({ message: 'No se encotraron usuarios' });
+  }
+  res.status(200).json(user);
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
+
 
